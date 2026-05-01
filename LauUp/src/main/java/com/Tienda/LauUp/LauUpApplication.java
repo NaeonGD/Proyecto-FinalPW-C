6db9c608e3a692b80bc58ab6fceb0021dbbc3667
NaeonGD <u20242227661@usco.edu.co>
2026-05-01 12:01:50 -0500

    Version Base
    Se Crea el inicializador de springBoot y se importa a eclipse IDE

---
+++ LauUp/src/main/java/com/Tienda/LauUp/LauUpApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda.LauUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauUpApplication.class, args);
+	}
+
+}
---
+++ LauUp/src/test/java/com/Tienda/LauUp/LauUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda.LauUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
