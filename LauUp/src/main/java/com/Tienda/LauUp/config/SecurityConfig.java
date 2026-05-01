b490ceca96cf21b1455a8bbd7f3d9e279d136a06
NaeonGD <u20242227661@usco.edu.co>
2026-05-01 17:34:39 -0500

    feat: add configuration package
    - Add config classes for application setup

---
+++ LauUp/src/main/java/com/Tienda/LauUp/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.Tienda.LauUp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http
+		.csrf(AbstractHttpConfigurer::disable)
+		.authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/", "/index.html", "/*.html",
+                        "/css/**", "/js/**", "/img/**",
+                        "/api/productos/**", "/api/categorias/**",
+                        "/api/usuarios/registro", "/api/usuarios/login"
+                    ).permitAll().anyRequest().authenticated()
+                //.anyRequest().authenticated() 
+                //dice que las demas peticiones requieren si o si estar logeado
+                )
+		.formLogin(AbstractHttpConfigurer::disable)
+		.httpBasic(AbstractHttpConfigurer::disable);
+		
+		return http.build();
+	}
+}
